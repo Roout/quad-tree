2f6975d4d6d8bc8af2ca780f60568bcd477d7d97
Roout <following2future@gmail.com>
2021-03-29 13:01:42 +0300

    fix(QuadTree): insert & find operations now trying to go as deep as possible first and then work with points vector belongs to the node; change division on quarters/cardinals base on SFML coordinate system

--- qtree/QuadTree.h
+++ qtree/QuadTree.h
@@ -10,12 +10,60 @@
 
 namespace tree {
 
+	/**
+	 * NE - corresponds to top-right quarter
+	 * SE - corresponds to bottom-right quarter
+	 * NW - corresponds to top-left quarter
+	 * SW - corresponds to bottom-left quarter
+	 */
 	enum Cardinals { NW = 0, NE, SW, SE, COUNT };
+
+	/**
+	 * Get quarter base where the point belongs base on following SFML coordinate system:
+	 * (0, 0) ----------- (W, 0)
+	 * ...
+	 * ...
+ 	 * (0, H) ----------- (W, H)
+	 */
+	constexpr Cardinals GetQuarter(const mt::Pt& point, const mt::Rect& box) noexcept {
+		Cardinals cardinal = Cardinals::NE;
+		if (point.x > box.GetMidX()) { // EAST
+			cardinal = point.y > box.GetMidY() ? Cardinals::SE : Cardinals::NE;
+		}
+		else { // WEST
+			cardinal = point.y > box.GetMidY() ? Cardinals::SW : Cardinals::NW;
+		}
+		return cardinal;
+	}
+
+	/**
+	 * Form rectangle from quarter on following SFML coordinate system:
+	 * (0, 0) ----------- (W, 0)
+	 * ...
+	 * ...
+	 * (0, H) ----------- (W, H)
+	 */
+	constexpr mt::Rect GetRect(Cardinals cardinal, const mt::Rect& box) noexcept {
+		switch (cardinal) {
+		case Cardinals::NE:
+			return { box.GetMidX(), box.GetMinY(), box.size.width / 2.f, box.size.height / 2.f };
+		case Cardinals::SE:
+			return { box.GetMidX(), box.GetMidY(), box.size.width / 2.f, box.size.height / 2.f };
+		case Cardinals::NW:
+			return { box.GetMinX(), box.GetMinY(), box.size.width / 2.f, box.size.height / 2.f };
+		case Cardinals::SW:
+			return { box.GetMinX(), box.GetMidY(), box.size.width / 2.f, box.size.height / 2.f };
+		default: assert(false && "Can't fallthrough here!");  break;
+		}
+
+		return box;
+	}
 
 	struct Node {
 		static constexpr size_t MAX_POINTS{ 2 };
 
 		std::array<Node*, Cardinals::COUNT> m_children{ nullptr };
+		// TODO: rewrite to std::array
 		std::vector<mt::Pt> m_data;
 		mt::Rect m_box{ {0.f, 0.f}, {0.f, 0.f} };
 	};
@@ -24,7 +72,7 @@ namespace tree {
 	public:
 		QuadTree(const mt::Rect& fullArea)
 			: m_root{ new Node {} }
-			, m_size{ 1 }
+			, m_size{ 0 }
 		{
 			m_root->m_box = fullArea;
 		}
@@ -101,6 +149,7 @@ namespace tree {
 			return m_size == 0;
 		}
 
+		// retrun number of points in the tree
 		size_t GetSize() const noexcept {
 			return m_size;
 		}
@@ -124,16 +173,17 @@ namespace tree {
 			if (!node->m_box.Contains(point)) {
 				return nullptr;
 			}
-			// check whether it contains in this node instead of children
-			if (auto it = std::find(node->m_data.cbegin(), node->m_data.cend(), point);
-				it != node->m_data.cend()
-				) {
-				return node;
-			}
+			
 			// find a needed quarter
-			const auto cardinal = this->GetQuarter(point, node->m_box);
+			const auto cardinal = GetQuarter(point, node->m_box);
 			if (auto child = node->m_children[cardinal]; child != nullptr) {
 				return this->Find(child, point);
+			}
+			else if (auto it = std::find(node->m_data.cbegin(), node->m_data.cend(), point);
+				it != node->m_data.cend()
+			) {
+				// point contains in this node
+				return node;
 			}
 			return nullptr;
 		}
@@ -146,34 +196,40 @@ namespace tree {
 				return false;
 			}
 
-			// see if the node still can accomodate any point
-			if (node->m_data.size() < Node::MAX_POINTS) {
-				node->m_data.push_back(point);
-				return true;
-			}
-
+			const auto cardinal = GetQuarter(point, node->m_box);
+			
 			// find a needed quarter
-			const auto cardinal = this->GetQuarter(point, node->m_box);
 			if (auto& child = node->m_children[cardinal]; child != nullptr) {
 				return this->Insert(child, point);
 			}
-			else {
-				child = new Node{};
-				child->m_box.origin = node->m_box.GetMid();
-				child->m_box.size = { node->m_box.size.width / 2.f,  node->m_box.size.height / 2.f };
+			else if (node->m_data.size() < Node::MAX_POINTS) {
+				// see if the node still can accomodate any point
+				node->m_data.push_back(point);
 				return true;
 			}
-		}
+			else {
+				child = new Node{};
+				child->m_box = GetRect(cardinal, node->m_box);
 
-		Cardinals GetQuarter(const mt::Pt& point, const mt::Rect& box) const noexcept {
-			Cardinals cardinal;
-			if (point.x > box.GetMidX()) { // EAST
-				cardinal = point.y > box.GetMidY() ? Cardinals::NE : Cardinals::SE;
+				// move points which have same quarter to this child node
+				size_t removed{ 0 };
+				const size_t size = node->m_data.size();
+				for (size_t i = 0; i + removed < size; ) {
+					if (child->m_box.Contains(node->m_data[i])) {
+						// copy to child quarter
+						child->m_data.push_back(node->m_data[i]);
+						// move to end of the vector points scheduled for removal
+						std::swap(node->m_data[i], node->m_data[size - removed - 1]);
+						// increase number of points to be removed
+						removed++;
+					}
+					else {
+						i++;
+					}
+				}
+				node->m_data.erase(node->m_data.cend() - removed, node->m_data.cend());
+				return this->Insert(child, point);
 			}
-			else { // WEST
-				cardinal = point.y > box.GetMidY() ? Cardinals::NW : Cardinals::SW;
-			}
-			return cardinal;
 		}
 
 	private:
